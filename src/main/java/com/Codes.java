e4a919c2a7ec28987730439189d3ada8a5267812
fc572 <francesco.calvino@gmail.com>
2017-02-01 12:08:37 +0000

    first commit of http Kodes

---
+++ src/main/java/com/Codes.java
@@ -0,0 +1,14 @@
+package com;
+
+class Codes {
+
+    private String message;
+
+    Codes(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
