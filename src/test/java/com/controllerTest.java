94583d091fcd74142fa84da3f1dcdcb15698f02d
Francesco Calvino <francescoc@asos.com>
2017-02-22 22:43:09 +0000

    trying to understand unit tests

---
+++ src/test/java/com/controllerTest.java
@@ -0,0 +1,40 @@
+package com;
+
+
+import com.controller.CodesController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class controllerTest {
+
+    @Mock
+    private Model modelmock;
+
+    @Before
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRoot() throws Exception {
+        CodesController codesController = new CodesController();
+        String responseCode = codesController.codes("204", modelmock);
+        Assert.assertEquals(responseCode, "codeDisplayPage");
+    }
+}
+
+/*
+{
+"message": "The code number 204 means No Content"
+}
+ */
