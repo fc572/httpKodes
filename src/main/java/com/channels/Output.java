866ac9ec84783895c3a838664dd193d0ba0af429
Francesco Calvino <fc572@users.noreply.github.com>
2022-06-24 11:45:28 +0100

    Code works

---
+++ src/main/java/com/channels/Output.java
@@ -0,0 +1,31 @@
+package com.channels;
+
+import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Output {
+
+    // Outbound channel adapter
+
+    // tag::messageSender[]
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
+        return new PubSubMessageHandler(pubsubTemplate, "fc-topic");
+    }
+    // end::messageSender[]
+
+    // tag::messageGateway[]
+    @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface PubsubOutboundGateway {
+
+        void sendToPubsub(String text);
+    }
+    // end::messageGateway[]
+}
